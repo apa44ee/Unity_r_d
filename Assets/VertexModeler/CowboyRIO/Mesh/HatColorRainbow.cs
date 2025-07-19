4565fe1b0f7a9722742c145f88fd544a7065ddc4
artemii.buchko <artemii.buchko@gmail.com>
2025-07-19 22:10:49 +0300

    no message

---
+++ Assets/VertexModeler/CowboyRIO/Mesh/HatColorRainbow.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class HatColorRainbow : MonoBehaviour
+{
+    public Renderer hatRenderer;
+    public float speed = 1f;
+
+    private Material hatMaterial;
+    private float hue = 0f;
+
+    void Start()
+    {
+        if (hatRenderer == null)
+        {
+            Debug.LogError("Hat Renderer не визначено!");
+            return;
+        }
+
+       
+        hatMaterial = hatRenderer.material;
+        StartCoroutine(Rainbow());
+    }
+
+    System.Collections.IEnumerator Rainbow()
+    {
+        while (true)
+        {
+            Color c = Color.HSVToRGB(hue, 1f, 1f);
+            hatMaterial.color = c;
+
+            hue += Time.deltaTime * speed;
+            if (hue > 1f) hue = 0f;
+
+            yield return null;
+        }
+    }
+}
